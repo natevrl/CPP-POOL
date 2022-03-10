585e06f4fd997f5b613f43d0e9503b0619d17185
v3r <v3rslor@protonmail.com>
2022-03-10 23:40:18 +0100

    learning ifstrean/ofstream

---
+++ day01/ex04/Filestream.hpp
@@ -0,0 +1,10 @@
+#ifndef FILESTREAM_H
+# define FILESTREAM_H
+
+
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#endif // ! FILESTREAM_H
--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -1,42 +1,41 @@
-#include <string>
-#include <string.h>
-#include <cstdio>
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-using namespace std;
+#include "Filestream.hpp"
 
 int main(int ac, char const *av[])
 {
-	string filestream, s1, s2, file, buf;
+	string filestream;
+	string s1, s2, file, buf, replace;
+	size_t i;
 
+	//handle args error
 	if (ac != 4)
 		return (cout << "needs 3 args" << endl, -1);
+
+	// init args
 	filestream = av[1];
 	s1 = av[2];
 	s2 = av[3];
-	ifstream readingStream(filestream);
 
+	// open filestream and handle error
+	std::ifstream readingStream;
+	readingStream.open(filestream.c_str());
 	if (!readingStream.is_open()) 
 		return (cerr << "open() error" << endl, -1);
 
+	//copying line by line filestream to file variable
 	while (getline(readingStream, buf))
 		file += buf + '\n';
-	std::cout << file;
-	cout << endl << file.find(s1) << endl;
 
+	//replace all s1 occurence on filestream by s2
+	buf.clear();
+	while ((i = file.find(s1)) != string::npos)
+		buf = file.erase(i, s1.length()).insert(i, s2);
 
-
+	// writing the buffer on the new file : <filestream>.replace
+	replace = ".replace";
+	ofstream writingStream(filestream.append(".replace").c_str());
+	writingStream << buf;
 
 	readingStream.close();
-
-
-		// while(readingStream >> s1)
-		// 	buf += s1 + '\n';
-		// cout << buf;
-		// readingStream.close();
-
-		// std::ofstream writingStream(filestream + ".replace");
-		// writingStream << "foo";
+	writingStream.close();
 	return 0;
 }
