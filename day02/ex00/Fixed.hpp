484dcdb309006ce71b58ef2bac1d68b0ed650ebc
v3r <v3rslor@protonmail.com>
2022-03-26 17:36:29 +0100

    learning polymorphism

---
+++ day02/ex00/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+ 
+
+
+class Fixed 
+{
+	public:
+		Fixed(void); // default constructor
+		~Fixed(void); // Destructor
+		Fixed (Fixed const &src); // Constructor per Copy
+		Fixed &operator=(Fixed const &rhs); // Operator Overload
+
+		 int getRawBits( void ) const;
+		 void setRawBits( int const raw );
+
+	private:
+		int _fixed_float;
+		static const int _bits;
+};
+
+#endif // !FIXED_H
