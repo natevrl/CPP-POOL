fb19cdeb13b172f2f1626cafd4628c0fd7b59337
Ubuntu <ubuntu@ip-172-26-6-65.eu-west-3.compute.internal>
2022-03-28 18:43:23 +0000

    learning overload operator

--- day02/ex01/Fixed.cpp
+++ day02/ex01/Fixed.cpp
@@ -1,19 +1,42 @@
 #include "Fixed.hpp"
 
-
+const int Fixed::_bits = 8;
 
 Fixed::Fixed(void) : _fixed_float( 0 ) 
 { 
 	std::cout << "Default constructor called" << std::endl;
 	return ;
-}; 
+} 
 
+Fixed::Fixed(const int x) 
+{
+	std::cout << "Int constructor called" << std::endl;
+	_fixed_float =  x << _bits;
+	return ;
+}
 
-Fixed::~Fixed(void) // Desctuctor  
+Fixed::Fixed( const float x ) 
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixed_float = roundf(x * (1 << _bits));
+	return ;
+}
+
+float Fixed::toFloat( void ) const
+{
+	return ( (float)_fixed_float / (1 << _bits) );
+}
+
+int Fixed::toInt( void ) const
+{
+	return ( _fixed_float / (1 << _bits) );
+}
+
+Fixed::~Fixed(void) // Destructor  
 { 
 	std::cout << "Destructor called" << std::endl;
 	return ; 
-}; 
+}
 
 Fixed::Fixed(Fixed const &src) // Copy Constructor
 { 
@@ -32,7 +55,7 @@ Fixed & Fixed::operator=(Fixed const &rhs) // Operator Overload =
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	// std::cout << "getRawBits member function called" << std::endl;
 	return this->_fixed_float;
 }
 
@@ -40,4 +63,10 @@ void Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->_fixed_float = raw;
+}
+
+std::ostream & operator<<(std::ostream & o, Fixed const & rhs )
+{
+	o << rhs.getRawBits();
+	return o;
 }
--- day02/ex01/Fixed.hpp
+++ day02/ex01/Fixed.hpp
@@ -4,23 +4,28 @@
 #include <cstdio>
 #include <iostream>
 #include <iomanip>
+#include <math.h>  
  
-
-
 class Fixed 
 {
 	public:
 		Fixed(void); // default constructor
+		Fixed(const int x); // Int Constructor
+		Fixed(const float x); // Float Constructor
 		~Fixed(void); // Destructor
 		Fixed (Fixed const &src); // Constructor per Copy
-		Fixed &operator=(Fixed const &rhs); // Operator Overload
+		Fixed & operator=(Fixed const &rhs); // Operator Overload
 
 		 int getRawBits( void ) const;
 		 void setRawBits( int const raw );
+		 float toFloat( void ) const;
+		 int toInt( void ) const;
 
 	private:
 		int _fixed_float;
 		static const int _bits;
 };
+
+std::ostream & operator<<(std::ostream & o, Fixed const & rhs );
 
 #endif // !FIXED_H
